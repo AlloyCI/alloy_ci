db35f202b6e513413ea3d799a39dc246c6eae90d
Patricio Cano <suprnova32@gmail.com>
2017-07-21 19:48:48 -0500

    Added missing helper functions

--- lib/alloy_ci/web/views/project_view.ex
+++ lib/alloy_ci/web/views/project_view.ex
@@ -3,6 +3,12 @@ defmodule AlloyCi.Web.ProjectView do
   alias AlloyCi.{Accounts, Projects}
   import Kerosene.HTML
 
+  def builds_chart(project) do
+    project
+    |> Chartable.builds_chart
+    |> Poison.encode!
+  end
+
   def has_github_auth(user) do
     case Accounts.github_auth(user) do
       nil -> false
@@ -12,5 +18,9 @@ defmodule AlloyCi.Web.ProjectView do
 
   def integration_url do
     Application.get_env(:alloy_ci, :integration_url)
+  end
+
+  def server_url do
+    Application.get_env(:alloy_ci, :server_url)
   end
 end
