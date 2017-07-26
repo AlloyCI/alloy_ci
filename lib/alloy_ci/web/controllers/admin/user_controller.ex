464927c1f52db4c3ce2abcc9e971426263288eda
Patricio Cano <suprnova32@gmail.com>
2017-07-26 18:48:10 -0500

    Allow admins to delete users

--- lib/alloy_ci/web/controllers/admin/user_controller.ex
+++ lib/alloy_ci/web/controllers/admin/user_controller.ex
@@ -1,7 +1,7 @@
 defmodule AlloyCi.Web.Admin.UserController do
   use AlloyCi.Web, :admin_controller
 
-  alias AlloyCi.{User, Repo}
+  alias AlloyCi.{Accounts, User, Repo}
 
   # Make sure that we have a valid token in the :admin area of the session
   # We've aliased Guardian.Plug.EnsureAuthenticated in our AlloyCi.Web.admin_controller macro
@@ -10,6 +10,13 @@ defmodule AlloyCi.Web.Admin.UserController do
   def index(conn, params, current_user, _claims) do
     {users, kerosene} = User |> Repo.paginate(params)
     render conn, "index.html", kerosene: kerosene, users: users, current_user: current_user
+  end
+
+  def delete(conn, %{"id" => id}, _, _) do
+    {:ok, _} = Accounts.delete_user(id)
+    conn
+    |> put_flash(:info, "User was deleted successfully")
+    |> redirect(to: admin_user_path(conn, :index))
   end
 
   def unauthenticated(conn, _params) do
