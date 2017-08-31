099fcf97603c46ab060a63cff2ba96acbf93b62a
Patricio Cano <suprnova32@gmail.com>
2017-08-31 17:09:10 -0500

    Saner defaults for test env

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,14 @@
 use Mix.Config
 
+# General application configuration
 config :alloy_ci,
-  github_api: AlloyCi.Github.Test
+  ecto_repos: [AlloyCi.Repo],
+  github_api: AlloyCi.Github.Test,
+  integration_id: "1",
+  integration_url: "https://github.com/alloy-ci",
+  private_key: "priv-key",
+  runner_registration_token: "lustlmc3gMl59smZ",
+  server_url: "https://alloy-ci.com"
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
@@ -11,6 +18,11 @@ config :alloy_ci, AlloyCi.Web.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+# Configures Notifiers
+config :alloy_ci, AlloyCi.Notifier,
+  slack: "false",
+  email: "false"
 
 # Configure your database
 config :alloy_ci, AlloyCi.Repo,
