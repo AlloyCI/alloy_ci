519a1091a8b15dbb53749fff16f1db7c45fa4ed1
Patricio Cano <suprnova32@gmail.com>
2017-08-31 12:39:57 -0500

    Implement Slack notifier and add Generic Notifier to handle the actual sending of notifications

---
+++ lib/alloy_ci/notifiers/notifier.ex
@@ -0,0 +1,46 @@
+defmodule AlloyCi.Notifier do
+  @moduledoc """
+  """
+  alias AlloyCi.{Notifiers, Notifications}
+  require Logger
+
+  def notify!(notification_id) do
+    notification_id
+    |> Notifications.get_with_project_and_user
+    |> send_notifications(config())
+  end
+
+  ###################
+  # Private functions
+  ###################
+  defp config do
+    :alloy_ci
+    |> Application.fetch_env!(__MODULE__)
+    |> Map.new
+  end
+
+  defp send_notifications(notification, %{email: "true", slack: "true"}) do
+    send_email(notification)
+    send_slack(notification)
+  end
+
+  defp send_notifications(notification, %{email: "true", slack: "false"}) do
+    send_email(notification)
+  end
+
+  defp send_notifications(notification, %{email: "false", slack: "true"}) do
+    send_slack(notification)
+  end
+
+  defp send_notifications(_, _) do
+    Logger.info("No notifiers have been configured. Skipping...")
+  end
+
+  defp send_email(notification) do
+    Notifiers.Email.send_notification(notification)
+  end
+
+  defp send_slack(notification) do
+    Notifiers.Slack.send_notification(notification)
+  end
+end
---
+++ lib/alloy_ci/notifiers/slack.ex
@@ -0,0 +1,50 @@
+defmodule AlloyCi.Notifiers.Slack do
+  @moduledoc """
+  """
+  import AlloyCi.Web.Router.Helpers
+
+  def send_notification(notification) do
+    HTTPoison.post(
+      config[:hook_url], build_payload(notification),
+      [{"content-type", "application/json"}]
+    )
+  end
+
+  ###################
+  # Private functions
+  ###################
+  defp base_notification_text(notification) do
+    "The pipeline with ID: #{notification.content["pipeline"]["id"]} for the branch *#{notification.content["pipeline"]["ref"] |> String.replace("refs/heads/", "")}*"
+  end
+
+  defp build_payload(notification) do
+    payload = %{
+      text: notification_text(notification),
+      channel: config[:channel],
+      username: config[:service_name]
+    }
+
+    Poison.encode!(payload)
+  end
+
+  defp config do
+    :alloy_ci
+    |> Application.fetch_env!(__MODULE__)
+    |> Map.new
+  end
+
+  defp notification_text(%{notification_type: "pipeline_failed"} = notification) do
+    base_notification_text(notification) <> " has failed. You can view the full trace log of the pipeline " <> "<#{pipeline_url(notification)}|here.>"
+  end
+
+  defp notification_text(%{notification_type: "pipeline_succeeded"} = notification) do
+    base_notification_text(notification) <> " has finished correctly. You can view the full trace log of the pipeline " <> "<#{pipeline_url(notification)}|here.>"
+  end
+
+  defp pipeline_url(notification) do
+    project_pipeline_url(
+      AlloyCi.Web.Endpoint, :show,
+      notification.project_id, notification.content["pipeline"]["id"]
+    )
+  end
+end
