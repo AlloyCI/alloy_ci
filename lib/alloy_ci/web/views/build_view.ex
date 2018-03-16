09d224e860cf9c2847906f2860836b275362cfcf
Patricio Cano <suprnova32@gmail.com>
2018-03-16 16:13:41 -0600

    Move build related view functions to where they belong, and added action to build show
    Closes #2

--- lib/alloy_ci/web/views/build_view.ex
+++ lib/alloy_ci/web/views/build_view.ex
@@ -1,7 +1,6 @@
 defmodule AlloyCi.Web.BuildView do
   use AlloyCi.Web, :view
   import AlloyCi.Web.ProjectView, only: [clean_ref: 1, ref_icon: 1, tags: 1]
-  import AlloyCi.Web.PipelineView, only: [build_duration: 1]
 
   def artifact_for(conn, %{artifacts: %{}, artifact: artifact} = build)
       when not is_nil(artifact) do
@@ -25,6 +24,38 @@ defmodule AlloyCi.Web.BuildView do
       "No artifacts were generated for this build."
     end
   end
+
+  def build_actions(conn, build, icon_class \\ "")
+  def build_actions(_, %{status: s}, _) when s in ~w(pending running created), do: ""
+
+  def build_actions(conn, build, icon_class) do
+    content_tag :span,
+      class: "float-right",
+      data: [toggle: "tooltip", placement: "bottom"],
+      title: "Restart this build" do
+      link to: project_build_path(conn, :create, build.project_id, %{id: build.id}),
+           method: :post,
+           class: "action-link" do
+        icon("refresh", icon_class)
+      end
+    end
+  end
+
+  def build_duration(%{finished_at: f, started_at: s}) when is_nil(f) or is_nil(s) do
+    "Pending"
+  end
+
+  def build_duration(build) do
+    (Timex.to_unix(build.finished_at) - Timex.to_unix(build.started_at))
+    |> TimeConvert.to_compound()
+  end
+
+  def build_status_icon("created"), do: icon("calendar", "fa-lg")
+  def build_status_icon("failed"), do: icon("close", "fa-lg")
+  def build_status_icon("pending"), do: icon("circle-o-notch", "fa-lg")
+  def build_status_icon("running"), do: icon("circle-o-notch", "fa-spin fa-lg")
+  def build_status_icon("success"), do: icon("check", "fa-lg")
+  def build_status_icon(_), do: icon("ban")
 
   def from_now(nil), do: "Never"
 
--- lib/alloy_ci/web/views/pipeline_view.ex
+++ lib/alloy_ci/web/views/pipeline_view.ex
@@ -1,37 +1,7 @@
 defmodule AlloyCi.Web.PipelineView do
   use AlloyCi.Web, :view
   alias AlloyCi.Projects
-
-  def build_actions(_, %{status: s}) when s in ~w(pending running created), do: ""
-
-  def build_actions(conn, build) do
-    content_tag :span,
-      class: "float-right",
-      data: [toggle: "tooltip", placement: "bottom"],
-      title: "Restart this build" do
-      link to: project_build_path(conn, :create, build.project_id, %{id: build.id}),
-           method: :post,
-           class: "action-link" do
-        icon("refresh", "fa-lg")
-      end
-    end
-  end
-
-  def build_duration(%{finished_at: f, started_at: s}) when is_nil(f) or is_nil(s) do
-    "Pending"
-  end
-
-  def build_duration(build) do
-    (Timex.to_unix(build.finished_at) - Timex.to_unix(build.started_at))
-    |> TimeConvert.to_compound()
-  end
-
-  def build_status_icon("created"), do: icon("calendar", "fa-lg")
-  def build_status_icon("failed"), do: icon("close", "fa-lg")
-  def build_status_icon("pending"), do: icon("circle-o-notch", "fa-lg")
-  def build_status_icon("running"), do: icon("circle-o-notch", "fa-spin fa-lg")
-  def build_status_icon("success"), do: icon("check", "fa-lg")
-  def build_status_icon(_), do: icon("ban")
+  import AlloyCi.Web.BuildView, only: [build_actions: 3, build_duration: 1, build_status_icon: 1]
 
   def can_manage?(pipeline, user) do
     Projects.can_manage?(pipeline.project_id, user)
