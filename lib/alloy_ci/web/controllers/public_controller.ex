ffbf61857c3a31a327a61084d4271b112a759f40
Patricio Cano <suprnova32@gmail.com>
2018-03-29 23:00:55 -0600

    Allow registration if one auth mechanism is missing

--- lib/alloy_ci/web/controllers/public_controller.ex
+++ lib/alloy_ci/web/controllers/public_controller.ex
@@ -1,5 +1,6 @@
 defmodule AlloyCi.Web.PublicController do
   use AlloyCi.Web, :controller
+  alias AlloyCi.Accounts
 
   plug(:put_layout, "public_layout.html")
   plug(:put_layout, "register_layout.html" when action in [:register])
@@ -9,7 +10,9 @@ defmodule AlloyCi.Web.PublicController do
   end
 
   def register(conn, _params, current_user, _claims) do
-    if current_user do
+    auths = Accounts.current_auths(current_user)
+
+    if Enum.count(auths) > 1 do
       redirect(conn, to: project_path(conn, :index))
     else
       render(conn, "register.html", current_user: current_user)
