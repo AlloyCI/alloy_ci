77e55fd97d6cc28739da45724e6ac992a15c6e07
Patricio Cano <suprnova32@gmail.com>
2018-08-28 15:15:30 -0500

    Use a TaskSupervisor when spawning the background processes

--- lib/alloy_ci/lib/background_scheduler.ex
+++ lib/alloy_ci/lib/background_scheduler.ex
@@ -1,18 +1,22 @@
 defmodule AlloyCi.BackgroundScheduler do
   @moduledoc """
   GenServer used to kick off background processes.
-  Functionality is basic for now, extra functionality might come in the future.
+  Functionality is basic for now, extra functionality, like storing which jobs
+  have been processed, or retry functionality might come in the future.
   """
   use GenServer
   require Logger
+  alias AlloyCi.TaskSupervisor
 
   # Client API
 
+  @spec start_link(any()) :: :ignore | {:error, any()} | {:ok, pid()}
   def start_link(state \\ %{}) do
     Logger.info("Starting background scheduler")
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
+  @spec push(any(), any()) :: :ok
   def push(worker, args) do
     GenServer.cast(__MODULE__, {:push, {worker, args}})
   end
@@ -20,17 +24,22 @@ defmodule AlloyCi.BackgroundScheduler do
   # Server API
 
   @impl true
+  @spec init(any()) :: {:ok, any()}
   def init(state) do
     {:ok, state}
   end
 
   @impl true
   def handle_cast({:push, {worker, args}}, state) do
-    Task.start(fn ->
-      Logger.info("Starting #{worker}")
-      worker.perform(args)
-      Logger.info("Finishing #{worker}")
-    end)
+    Task.Supervisor.start_child(
+      TaskSupervisor,
+      fn ->
+        Logger.info("Starting #{worker}")
+        worker.perform(args)
+        Logger.info("Finishing #{worker}")
+      end,
+      restart: :transient
+    )
 
     {:noreply, state}
   end
