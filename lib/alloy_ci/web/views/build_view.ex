08a467a942ef1e07f06da37064595e2671a5b9dd
Patricio Cano <suprnova32@gmail.com>
2018-08-28 15:17:24 -0500

    Import get_trace function

--- lib/alloy_ci/web/views/build_view.ex
+++ lib/alloy_ci/web/views/build_view.ex
@@ -1,7 +1,7 @@
 defmodule AlloyCi.Web.BuildView do
   use AlloyCi.Web, :view
   import AlloyCi.Web.ProjectView, only: [ref_icon: 1, tags: 1]
-  import AlloyCi.Builds, only: [clean_ref: 1]
+  import AlloyCi.Builds, only: [clean_ref: 1, get_trace: 1]
 
   def artifact_for(conn, %{artifacts: %{}, artifact: artifact} = build)
       when not is_nil(artifact) do
